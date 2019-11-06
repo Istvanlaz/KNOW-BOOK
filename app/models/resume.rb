86b42f436b812ba12cbf903e4cffb314b9a9a7cd
Istvan Lazar <istvanlaz@gmail.com>
2019-11-06 13:52:45 +0100

    done debugging zat shit

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,7 +1,7 @@
 class Resume < ActiveRecord::Base
 
-  mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
+  mount_uploader :attachment, AttachmentUploader
   has_one_attached :attachment
 
-  validates :name, presence: true # Make sure the owner's name is present.
+  validates :name, presence: true
 end
