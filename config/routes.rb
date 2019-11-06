dc46966dd7632e192d4e0e6a9a64df122ea726ae
harald doultremont <haralddou@hotmail.com>
2019-11-06 14:21:34 +0100

    fixing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :resumes, only: [:index, :new, :create, :destroy]
-  root "resumes#index"
   resources :books
+  root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
