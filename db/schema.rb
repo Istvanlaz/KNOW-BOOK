948614ea07ee1e2d5ba9b10c026565f0387bf194
Gauthier Wolters <gauthier.wolters@skynet.be>
2019-11-08 13:32:50 +0100

    fixed avatar issue

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,13 @@ ActiveRecord::Schema.define(version: 2019_11_07_135949) do
     t.index ["user_id"], name: "index_readings_on_user_id"
   end
 
+  create_table "resumes", force: :cascade do |t|
+    t.string "name"
+    t.string "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
